b5ffa40b155bf378bace57d0f55a335631dcbb09
Cherakadavath <Saneej.Cherakadavath@provident.co.uk>
2017-06-15 11:09:27 +0100

    settings page added

---
+++ src/app/settings/setting.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+
+@Component({
+  selector: 'app-setting',
+  templateUrl: './setting.component.html',
+  styleUrls: ['./setting.component.css']
+})
+export class SettingComponent implements OnInit {
+
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+
+
+  }
+
+ 
+
+}
--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { SettingComponent } from '../settings/setting.component';
+import { HeaderModule } from '../header/header.module';
+
+
+
+const settingsRouting: Routes = [
+  { path: 'settings', component: SettingComponent }
+
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    HeaderModule,
+    RouterModule.forChild(settingsRouting)
   ],
-  declarations: []
+  declarations: [
+    SettingComponent
+  ]
 })
 export class SettingsModule { }
